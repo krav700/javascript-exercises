a80f1740998590d4e0f14d38e26fb504d622a40d
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2025-02-26 19:17:29 +0200

    all exercises complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  return +((degrees - 32.0) * 5/9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  return +((degrees * 9/5) + 32.0).toFixed(1);
 };
 
 // Do not edit below this line
